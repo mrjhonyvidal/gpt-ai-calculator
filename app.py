c7bfdd8a997a9735008458f9274a45918fd2f733
jvo <mrjhonyvidal@gmail.com>
2024-02-04 23:55:27 +0000

    Add how api parameters are accessible

--- app.py
+++ app.py
@@ -118,7 +118,7 @@ def main():
             ]
 
     for msg in st.session_state.messages:
-        st.chat_message(msg["role"]).write(msg["content"])
+        st.chat_message(msg.role).write(msg.content)
 
     if prompt := st.chat_input():
         if not openai_api_key:
