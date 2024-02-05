a974ea5ed2714a789ff5c243e09a1394f404229a
jvo <mrjhonyvidal@gmail.com>
2024-02-05 00:30:08 +0000

    Fix appending

--- app.py
+++ app.py
@@ -118,7 +118,7 @@ def main():
             ]
 
     for msg in st.session_state.messages:
-        st.chat_message(msg.role).write(msg.content)
+        st.chat_message(msg["role"]).write(msg["content"])
 
     if prompt := st.chat_input():
         if not openai_api_key:
@@ -129,9 +129,9 @@ def main():
         st.session_state.messages.append({"role": "user", "content": prompt})
         st.chat_message("user").write(prompt)
         response = openai.chat.completions.create(model=selected_model_name, messages=st.session_state.messages)
-        msg = response.choices[0].message
-        st.session_state.messages.append(msg)
-        st.chat_message("assistant").write(msg.content)
+        msg = response.choices[0].message.content
+        st.session_state.messages.append({"role": "assistant", "content": msg})
+        st.chat_message("assistant").write(msg)
 
 if __name__ == "__main__":
     main()
